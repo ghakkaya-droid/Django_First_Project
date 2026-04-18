61c40ec6735f9b6921fa408d8e00dd72b62f279a
Haluk Akkaya <ghakkaya@gmail.com>
2026-04-18 11:19:56 +0500

    Django Routing Learned

--- first_app/urls.py
+++ first_app/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("index", views.index, name="index"),
-    #path("python_course", views.python_course, name="python_course"),
-    #path("java_course", views.java_course, name="java_course"),
-    path("<str:item>", views.course, name="course")
+    path("",views.index,name="main"),
+    path("<int:num1>",views.course_number_view,name="course_number"),
+    #path("python_course", views.python_course, name="python_course"), #***webSite.com/first_app/python_course
+    #path("java_course", views.java_course, name="java_course"), #***webSite.com/first_app/java_course
+    path("<str:item>", views.course, name="course"), #***webSite.com/first_app/"değişken"
+    path("<int:num1>/<int:num2>/",views.multiply_view,name="multiply"), #***webSite.com/first_app/"Sayı-1"/"Sayı-2"/
+     
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404, HttpResponseNotFound,HttpResponseRedirect
+from django.urls import reverse
 course_dictionary = {
     "python": "Python Course Page",
     "java": "Java Course Page",
@@ -9,11 +9,29 @@ course_dictionary = {
 }
 
 def course(request,item):
-    return HttpResponse(course_dictionary.get(item,"Not Found"))
+    try:
+        course = course_dictionary[item]
+        return HttpResponse(course)
+    except:
+        return HttpResponseNotFound ("Not found! - Please look for another course!")
+        #raise Http404("Not found! - Please look for another course!")
+    #return HttpResponse(course_dictionary.get(item,"Not Found"))
 
 
 def index(request):
     return HttpResponse("This is first Django project, first index")
+
+def multiply_view(request,num1,num2):
+    return HttpResponse(f"Result = {num1*num2}")
+
+def course_number_view(request,num1):
+    course_list = list(course_dictionary.keys())
+    try:
+        course = course_list[num1]
+        page_to_go=reverse("course",args=[course])
+        return HttpResponseRedirect(page_to_go)
+    except:
+        return HttpResponseNotFound ("Not found! - Please look for another course!")
 
 #def python_course(request):
 #    return HttpResponse("Python Course Page")
